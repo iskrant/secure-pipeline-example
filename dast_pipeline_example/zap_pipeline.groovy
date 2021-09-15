c2ad8de3c1417404e25d8e2eedc09938602937be
Skorikov Anton <skorikov.anton@gmail.com>
2021-09-15 18:00:03 +0300

    FIX(DAST): replace webgoat-8.0 to goatandwolf:v8.2.1 and zap-url.

--- dast_pipeline_example/zap_pipeline.groovy
+++ dast_pipeline_example/zap_pipeline.groovy
@@ -1,24 +1,25 @@
 node {
 
-  script {
-    try {
-      // Context import fails if it already exists
-      sh 'zap-cli --zap-url zap -p 8000 --api-key 5364864132243598723485 --port 8000 context import /zap/data/WebGoat.context'
-    }
-    catch (Exception e) {
-    }
-  }
+// Only once
+  // script {
+  //   try {
+  //     // Context import fails if it already exists
+  //     sh 'zap-cli --zap-url http://zap -p 8000 --api-key 1234567132243598543210 --port 8000 context import /zap/data/WebGoat.context'
+  //   }
+  //   catch (Exception e) {
+  //   }
+  // }
 
   script {
     try {
       // If it finds results, returns error code, but we still want to publish the report
-      sh 'zap-cli --zap-url zap -p 8000 --api-key 5364864132243598723485 quick-scan -c WebGoat -u tester -s all --spider -r http://webgoat:8080/WebGoat'
+      sh 'zap-cli --zap-url http://zap -p 8000 --api-key 1234567132243598543210 quick-scan -c WebGoat -u tester -s all --spider -r http://webgoat:8080/WebGoat'
     }
     catch (Exception e) {
     }
   }
 
-  sh 'zap-cli --zap-url zap -p 8000 --api-key 5364864132243598723485 report -o zap_report.html -f html'
+  sh 'zap-cli --zap-url http://zap -p 8000 --api-key 1234567132243598543210 report -o zap_report.html -f html'
 
   publishHTML([
     allowMissing: false, 
